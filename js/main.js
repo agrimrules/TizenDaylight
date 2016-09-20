8d06ad3267bc0064beadef01889854d7eaaebc5d
Agrim Asthana <agrimasthana@gmail.com>
2016-09-20 00:33:31 -0500

    using async calls to manage apis

--- js/main.js
+++ js/main.js
@@ -6,35 +6,6 @@ window.onload = function() {
             } catch (ignore) {}
         }
     });
-
-    /**
-     * Gets the location based on IP.
-     * */
-    function getIPLocation() {
-    	var xmlHttp = new XMLHttpRequest();
-    	
-    	xmlHttp.overrideMimeType("application/json");
-    	xmlHttp.open("GET","http://ip-api.com/json/", false);
-    	xmlHttp.onreadystatechange = function(){
-    		if(xmlHttp.readyState === 4){
-    			 return JSON.parse(xmlHttp.responseText);
-    		}
-    		else {
-    			return null;
-    		}
-    	};
-    	try{
-    	xmlHttp.send();
-    	}
-    	catch(e){
-    	var tl =	document.querySelector("#content-Sunrise");
-    	var bl =	document.querySelector("#content-Sunset");
-    	tl.innerHTML = "No Internet";
-    	bl.innerHTML = "Access!";
-    	}
-    	return xmlHttp.onreadystatechange();
-    }
-    
     /**
      * Gets The Sunrise and sunset based on location.
      */
@@ -42,23 +13,36 @@ window.onload = function() {
     	var xmlHttp = new XMLHttpRequest(),
     	elSunset = document.querySelector("#content-Sunset"),
     	elSunrise = document.querySelector("#content-Sunrise"),
-    	IPinfo,
+    	lat,
+    	lon,
     	SunRes,
     	sunrise,
     	sunset;
-    	IPinfo = getIPLocation();
-    	xmlHttp.overrideMimeType("application/json");
-    	xmlHttp.open("GET","http://api.sunrise-sunset.org/json?lat="+IPinfo.lat+"&lng="+IPinfo.lon+"&formatted=0", false);
-    	xmlHttp.onreadystatechange = function () {
-    		if(xmlHttp.readyState === 4) {
-    			SunRes = JSON.parse(xmlHttp.responseText);
-    			sunrise = new tizen.TZDate(new Date(SunRes.results.sunrise),tizen.time.getLocalTimezone()).toLocaleTimeString();
-    			sunset = new tizen.TZDate(new Date(SunRes.results.sunset),tizen.time.getLocalTimezone()).toLocaleTimeString();
-    			elSunset.innerHTML = 'Sunset: '+sunset;
-    			elSunrise.innerHTML = 'Sunrise: '+sunrise;
+    	xmlHttp.onload = function(){
+    		if(xmlHttp.status === 200 && xmlHttp.readyState === 4) {
+    			elSunrise.innerHTML = 'Loading';
+    			elSunset.innerHTML = 'Data';
+    			var res = JSON.parse(xmlHttp.responseText);
+    			lat = res.lat;
+    			lon = res.lon;
+    			var xmlHttp2 = new XMLHttpRequest();
+    			xmlHttp2.onload = function() {
+    				if(xmlHttp2.status === 200 && xmlHttp2.readyState === 4) {
+    					SunRes = JSON.parse(xmlHttp2.responseText);
+    					sunrise = new tizen.TZDate(new Date(SunRes.results.sunrise),tizen.time.getLocalTimezone()).toLocaleTimeString();
+    					sunset = new tizen.TZDate(new Date(SunRes.results.sunset),tizen.time.getLocalTimezone()).toLocaleTimeString();
+    					elSunset.innerHTML = 'Sunset: '+sunset;
+    					elSunrise.innerHTML = 'Sunrise: '+sunrise;
+    				}    		
+    			};
+    			xmlHttp2.open("GET","http://api.sunrise-sunset.org/json?lat="+lat+"&lng="+lon+"&formatted=0", true);
+    			xmlHttp2.send();
     		}
     	};
+    	xmlHttp.overrideMimeType("application/json");
+    	xmlHttp.open("GET","http://ip-api.com/json/", true);
     	xmlHttp.send();
+    	
     }
     
     getTimeSpan();
