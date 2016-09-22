4ea9b9574da11f2bf9d3838ecb58511ec93fc3ec
Agrim Asthana <dev@agrimasthana.com>
2016-09-22 17:35:38 -0500

    Removed unwanted files. Changed sunrise icon color. Handling back button to exit

--- js/daylight.js
+++ js/daylight.js
@@ -1,21 +1,34 @@
+//Handle back button to exit
+document.addEventListener('tizenhwkey', function(e) {
+        if (e.keyName === "back") {
+            try {
+                tizen.application.getCurrentApplication().exit();
+            } catch (ignore) {}
+        }
+    });
+
 angular.module('myApp',[])
-.controller('myCtrl', function($scope, $http, $route, $timeout){
+.controller('myCtrl', function($scope, $http, $timeout){
 	$scope.sunrise = "loading";
 	$scope.sunset = "data";
 	$http.get('http://ip-api.com/json/').then(function(res){
 		$http.get('http://api.sunrise-sunset.org/json?lat='+res.data.lat+'&lng='+res.data.lon+'&formatted=0').then(function(res){
-			$scope.sunrise = new tizen.TZDate(new Date(res.data.results.sunrise),tizen.time.getLocalTimezone()).toLocaleTimeString();
-			$scope.sunset = new tizen.TZDate(new Date(res.data.results.sunset),tizen.time.getLocalTimezone()).toLocaleTimeString();
+			$scope.sunrise = returnformatteddate(res.data.results.sunrise);
+			$scope.sunset = returnformatteddate(res.data.results.sunset);
 		}, function(err) {
 			$scope.sunrise = 'error';
 			console.log(err);
 		});
 	}, function(err){
-		$scope.sunrise = 'out error';
+		$scope.sunrise = 'error';
 	});
 	
+	function returnformatteddate(datestring){
+		return new tizen.TZDate(new Date(datestring),tizen.time.getLocalTimezone()).toLocaleTimeString();
+	}
+	
 	var reloadPage = function() {
-		  $route.reload();
+		  location.reload();
 		};
-	$timeout(reloadPage, 3000);
+	$timeout(reloadPage, 10000);
 });
